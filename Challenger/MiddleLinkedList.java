2dfe9986ad3b63ed197940bb9a67597564e3ba6b
PhillByrd2018 <Phill.e.byrd@gmail.com>
2020-04-08 10:37:43 -0700

    List Middle Node Challenge

---
+++ Challenger/MiddleLinkedList.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class MiddleLinkedList {
+
+    public static int[] testCase1 = {1,2,3,4,5};
+    public static int[] testCase2 = {1,2,3,4,5,6};
+
+    public static ListNode middleNodeGreedy(LinkedList<Integer> head) {
+
+        
+        int numOfNodes = head.size();
+
+        int middle = numOfNodes / 2;
+
+        ListNode ans = new ListNode(head.get(middle));
+
+        return ans;
+        
+    }
+
+    public static LinkedList<Integer> makeListNodes(int[] nums){
+       
+        LinkedList<Integer> response = new LinkedList<>();
+        for (int num : nums) {
+            response.add(num);
+        }
+        return response;
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> head = makeListNodes(testCase1);
+        System.out.println("Expected: 3 :: Actual: " + middleNodeGreedy(head).val);
+
+        LinkedList<Integer> head2 = makeListNodes(testCase2);
+        System.out.println("Expected: 4 :: Actual: " + middleNodeGreedy(head2).val);
+        
+    }
+
+}
+
+
