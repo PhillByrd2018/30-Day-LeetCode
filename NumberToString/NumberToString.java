6b16e4cd04a14eda8ff99ab29f58789c483019ea
PhillByrd2018 <Phill.e.byrd@gmail.com>
2020-04-09 12:53:56 -0700

    Slick Solution added

---
+++ NumberToString/NumberToString.java
@@ -0,0 +1,25 @@
+
+public class NumberToString {
+    // https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/481/
+
+    public static int testCase1 = 315;
+
+    public static String convertNumToString(int num){
+        String ans = "";
+
+        String numString = String.valueOf(num);
+        int length = numString.length();
+
+        for (int i = 0; i < length; i++) {
+
+        }
+
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println("Expected Three Hundred and fifteen :: Actual: " + convertNumToString(testCase1));
+    }
+}
