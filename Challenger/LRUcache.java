80d0af140194350895d7dced5fdc26882221f379
PhillByrd2018 <Phill.e.byrd@gmail.com>
2020-04-24 12:32:06 -0700

    LinkedHashMap impl

---
+++ Challenger/LRUcache.java
@@ -0,0 +1,35 @@
+package Challenger;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Your LRUCache object will be instantiated and called as such: LRUCache obj =
+ * new LRUCache(capacity); int param_1 = obj.get(key); obj.put(key,value);
+ */
+
+public class LRUcache extends LinkedHashMap<Integer, Integer>{
+
+    private int capacity;
+    
+    // public LRUCache(int capacity) {
+    //     this.super(capacity, 0.75F, true);
+    //     this.capacity = capacity;
+    // }
+    
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+    
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity; 
+    }
+    public static void main(String[] args) {
+        
+    }
+}
