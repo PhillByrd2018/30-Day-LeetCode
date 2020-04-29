960c78548799253f648529edf7c63ca438967114
PhillByrd2018 <Phill.e.byrd@gmail.com>
2020-04-28 17:54:30 -0700

    Defang an IP into new IP

---
+++ Challenger/DefangIP.java
@@ -0,0 +1,27 @@
+package Challenger;
+
+import jdk.nashorn.internal.ir.ReturnNode;
+
+public class DefangIP {
+
+    public static String defangIPaddrReplaceAll(String address) {
+        return address.replace(".", "[.]");
+    }
+
+    public static String defangIPaddr(String address) {
+        StringBuilder ans = new StringBuilder();
+        for (char c: address.toCharArray()){
+            if (c != '.') {
+                ans.append(c);
+            } else { 
+                ans.append("[.]");
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(defangIPaddrReplaceAll("1.1.1.1"));
+        System.out.println(defangIPaddr("1.1.1.1"));
+    }
+}
