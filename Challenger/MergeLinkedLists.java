a630466d9cee0876217c878b7ef06a40ffeea01c
PhillByrd2018 <Phill.e.byrd@gmail.com>
2020-05-20 12:28:49 -0700

    update testCases

--- Challenger/MergeLinkedLists.java
+++ Challenger/MergeLinkedLists.java
@@ -51,6 +51,35 @@ public class MergeLinkedLists {
         return ansHeadHolder.next;
     }
 
+    public static ListNode mergeTwoListsOptomized(ListNode l1, ListNode l2) {
+
+        // validations
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode preHeadAns = new ListNode(0);
+        ListNode currentIndex = preHeadAns;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                currentIndex.next = l1;
+                l1 = l1.next;
+            } else {
+                currentIndex.next = l2;
+                l2 = l2.next;
+            }
+            currentIndex = currentIndex.next;
+        }
+
+        currentIndex.next = (l1 == null) ? l2 : l1;
+
+        return currentIndex.next;
+    }
+
     public static void main(String[] args) {
 
         ListNode testCaseHeadNodeL = new ListNode(1);
@@ -60,9 +89,6 @@ public class MergeLinkedLists {
 
         ListNode testCaseL2 = new ListNode(4);
         testCaseL1.next = testCaseL2;
-
-
-
 
         ListNode testCaseR = new ListNode(1);
 
